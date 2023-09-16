e085794379b86b562434ef62b7dab4f484c1a0de
Kavi R <62686489+Kaviarasan-R@users.noreply.github.com>
2023-09-16 20:52:38 +0530

    Update app.service.ts

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,14 +1,8 @@
-import { Injectable, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-@Module({
-  imports: [ConfigModule],
-})
 export class AppService {
-  constructor(private configService: ConfigService) {}
-
   getHello(): string {
-    return this.configService.get<string>('MESSAGE');
+    return "Hello World!";
   }
 }
